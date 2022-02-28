b53cbae123088505b20a882a15ae23f4a62a440c
raphael anderson <raphael.8379@hotmail.com>
2022-02-28 07:50:44 -0300

    A1-criei o arquivo index.php

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WLQ8F2CPMS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WLQ8F2CPMS');
+</script>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css" />
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="favicon/site.webmanifest">
+<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+<!-- icones da aba servicos -->
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
+
+<title>Raphael Anderson RA</title>
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+<div class="container-fluid">
+
+<div class="row">
+    <div id="logo"> 
+        <img src="logo.png" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+
+            <div class="col-12 col-sm-6"> 
+               <div id="texto_topo"> 
+               <h1>  Criação de sites e Aplicativos personalizados </h1>
+                </div>
+            </div>
+
+            <div class="col-12 col-sm-6"> 
+                        <img src="intro-mobile.png" class="img-fluid">
+            </div>
+</div>            
+
+        <div align="center" style="padding-top: 100px;">  <h1> Nossos Serviços  </h1> </div>
+
+      
+                <div class="row"> <!-- Comeco  Row servicos -->
+                    <div class="servicos">
+
+                        <card>
+                                
+                            <section2>
+                                <div align="center" style="padding-top:25px;">  
+                                <i class='fas fa-mobile-alt' style='font-size:36px'></i>                       
+                                <h4 align="center" style="font-style:italic;  padding-top:10px; font-size:24px;">   Desenvolvimento de APP </h4>
+                               
+                                </div>  
+                                </section2>
+                                
+                                <section2>
+                                <div align="center" style="padding-top:25px;"> 
+                                <i class='fas fa-palette' style='font-size:36px'></i>
+                                <h4 align="center" style="font-style:italic;  padding-top:10px; font-size:24px;">   UX UI Design </h4> 
+                                      
+                                </div>
+                                </section2>
+                                
+                                <section2>
+                                <div align="center" style="padding-top:25px;"> 
+                                <i class='fas fa-desktop' style='font-size:36px'></i>
+                                <h4 align="center" style="font-style:italic;  padding-top:10px; font-size:24px;">  Sites personalizados </h4> 
+                                    
+                                </div>
+                        </section2>
+
+
+                        </card>          
+                    
+                    </div>   <!-- Final DIV servicos -->    
+                </div> <!-- Final Row servicos -->
+            
+
+
+</div>   <!-- Final container fluid -->
+                                                    
+
+      
+          
+            
+           
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+               	    
+                
+</body>
+</html>
